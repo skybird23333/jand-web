b71412fd8d17b3eab77aaf5a57d989a15198551d
skybird23333 <2603003199a@gmail.com>
2022-09-25 00:44:37 +0800

    Feat App/Rest: Cover process endpoints

--- app/http/client.js
+++ app/http/client.js
@@ -9,4 +9,20 @@ export default class Client {
     async getAllProcess() {
         return await this.rest.get('/process/all')
     }
+
+    async getProcess(name) {
+        return await this.rest.get(`/process/${name}`)
+    }
+
+    async editProcessRunningConfig(name, config) {
+        return await this.rest.patch(`/process/${name}/edit`, config)
+    }
+
+    async stopProcess(name) {
+        return await this.rest.post(`/process/${name}/stop`)
+    }
+
+    async restartProcess(name) {
+        return await this.rest.post(`/process/${name}/restart`)
+    }
 }
