1071ef380503bfb2c77112c8e233629fc7a673cb
skybird23333 <2603003199a@gmail.com>
2022-09-24 21:02:17 +0800

    Feat App: Update process style and link

--- app/components/Home/ProcessComponent.vue
+++ app/components/Home/ProcessComponent.vue
@@ -7,41 +7,92 @@ export default {
     },
     props: [
         'process'
-    ]
+    ],
+    data() {
+        return {
+            color: 'blue'
+        }
+    },
+    mounted() {
+        this.determineColor()
+    },
+    methods: {
+        determineColor() {
+            this.process.Running ? this.$data.color = 'green' : this.$data.color = 'red'
+        },
+        onClicked() {
+            this.$router.push(`/process/${this.process.Name}`)
+        }
+    }
 }
 </script>
 <template>
-    <Card background color="green">
+    <Card background :color="color" class="process" @click="onClicked">
 
-        <span>
+        <template #header>
             <b>{{ process.Name }}</b>
-            <span v-if="process.Running" class="tag success">Running</span>
-            <span v-else class="tag error">Stopped</span>
+            <code class="code">
+                {{ process.Filename }}
+                {{ process.Arguments.join(' ') }}
+            </code>
+        </template>
 
-            <div class="card-footer" style="font-size: small; font-weight: normal;">
+
+        <div class="info-grid">
+            <span class="working-directory">
                 {{ process.WorkingDirectory }}
-                <code class="code">
-                    {{ process.Filename }}
-                    {{ process.Arguments.join(' ') }}</code>
-            </div>
-        </span>
-
-        <div style="display:grid; grid-template-columns: auto max-content;">
-            <div style="display: block; margin-top: 10px">
+            </span>
+            <div style="display: block;">
+                <span class="label" v-if="process.ProcessId != -1">
+                    PID {{ process.ProcessId }}
+                </span>
                 <span class="label">
                     Auto restart {{ process.AutoRestart ? 'enabled' : 'disabled' }}
                 </span>
                 <span class="label">
                     Restarted {{ process.RestartCount }} times
                 </span>
-                <span class="label">
-                    PID {{ process.ProcessId }}
-                </span>
-            </div>
-            <div>
-                <div class="button primary">Restart</div>
-                <div class="button danger">Stop</div>
             </div>
         </div>
     </Card>
 </template>
+
+<style scoped>
+
+.process {
+    transition: border-color 0.2s, background-color 0.2s;
+    filter: drop-shadow(1px 1px 8px black);
+}
+
+.process:hover {
+    background-color: var(--background-tertiary);
+    border-color: var(--foreground-primary);
+    filter: drop-shadow(3px 3px 8px black);
+    cursor: pointer;
+}
+
+.code {
+    background-color: var(--background-primary);
+    margin: 4px;
+}
+
+.info-grid {
+    display: grid;
+    grid-template-columns: auto max-content;
+}
+
+.working-directory {
+    font-size: small;
+    font-weight: normal;
+}
+
+@media only screen and (max-width: 600px) {
+    .info-grid {
+        grid-template-columns: max-content;
+    }
+
+    .working-directory {
+        display: none
+    }
+}
+</style>
