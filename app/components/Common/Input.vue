fc331fea14e32cdec094b815d24c80c3eeac5cae
skybird23333 <2603003199a@gmail.com>
2022-09-24 23:53:49 +0800

    Add Input component

---
+++ app/components/Common/Input.vue
@@ -0,0 +1,12 @@
+<template>
+    <input class="input" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+        :placeholder="placeholder" :type="type" :disabled="disabled" :required="required">
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    props: ['modelValue', 'placeholder', 'type', 'disabled', 'required'],
+    emits: ['update:modelValue'],
+};
+</script>
