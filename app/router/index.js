872e4ef74cd1f86ca54fdc616fb738fece8a025a
skybird23333 <2603003199a@gmail.com>
2022-09-26 20:18:49 +0800

    Fix: Make app not lazy-load ProcessView
    If the component is lazy loaded, the page will appear unresponsive
    when opening a proceess from the home page.

--- app/router/index.js
+++ app/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProcessView from '../views/ProcessView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
         {
             path: '/process/:name',
             name: 'process',
-            component: () => import('../views/ProcessView.vue')
+            component: ProcessView
         }
     ]
 })
