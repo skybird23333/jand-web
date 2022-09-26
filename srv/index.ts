fedf39220a331ce783a6984c4f2dff7c16ee2635
skybird23333 <2603003199a@gmail.com>
2022-09-26 23:08:52 +0800

    Fix Srv: Fallback to index.html

--- srv/index.ts
+++ srv/index.ts
@@ -1,12 +1,15 @@
 import jand from 'jand-ipc'
 import express from 'express'
 import { apiRouter } from './routes'
+import fallback from 'express-history-api-fallback'
 
 const app = express()
 app.set('view engine', 'ejs')
 
 app.use('/', express.static('dist-app'))
 app.use('/api', apiRouter)
+
+app.use(fallback('index.html', { root: 'dist-app' }))
 
 async function run() {
     await jand.connect()
